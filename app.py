74cfaf3ab7dc339d530e8cf42afdd04ff626b634
bethsreview <rbeththomas21@gmail.com>
2023-06-05 21:47:08 +0100

    adding compare function
    added logic for comparing carbon footprint estimate. changed index.html to display comparison as a result

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ import pandas as pd
 app = Flask(__name__)
 
 data = {
-    'Activity': ['Driving', 'Flying', 'Biking', 'Walking'],
-    'CO2_Emission': [0.24, 0.15, 0.008, 0.001]
+    'Activity': ['TV', 'Driving', 'Flying', 'Biking', 'Walking'],
+    'CO2_Emission': [0.001, 0.24, 0.15, 0.008, 0.001]
+}
+emission_dict = {
+    'Swimming': 0.01,
+    'Driving' : 0.24,
+    'Flying' : 0.15,
+    'Biking' : 0.008,
+    'Walking':0.001
+
 }
 emissions_df = pd.DataFrame(data)
 
 def estimate_carbon_footprint(activity, distance, people=1):
-    emission = emissions_df.loc[emissions_df['Activity'] == activity, 'CO2_Emission'].values[0]
+    #emission = emissions_df.loc[emissions_df['Activity'] == activity, 'CO2_Emission'].values[0]
+    emission = emission_dict.get(activity,None)
+    print(emission)
     carbon_footprint = emission * distance * people
     return carbon_footprint
 
@@ -23,9 +33,29 @@ def calculate():
     activity = request.form["activity"]
     distance = float(request.form["distance"])
     people = int(request.form["people"])
-    
-    carbon_footprint = estimate_carbon_footprint(activity, distance, people)
-    return jsonify(carbon_footprint=carbon_footprint)
+    result = compare(activity, distance, people)
+    return jsonify(carbon_footprint= result)
+
+def compare(activity, distance, people=1):
+    """ 
+    calculate the CO2 emission for each
+    compare the values
+    take the least value
+    print the least vaue
+    """ 
+    choosen_value =  estimate_carbon_footprint(activity, distance, people)
+    least_value = choosen_value
+    green_activity = activity
+    for key,value in emission_dict.items():
+          print("***{}***{}***".format(key,value))
+          carbon_footprint =estimate_carbon_footprint(key, distance, people)
+          if carbon_footprint < least_value:
+              least_value = carbon_footprint
+              green_activity = key
+    output = "Carbon footprint for {} is {} kg CO2. Most green activity is {}, with carbon footprint {}".format(activity,choosen_value,green_activity,least_value)
+    return output
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
